155ec211156ab97fd5d51377c763df583d73453d
timn <i-wont-tell@you.com>
2024-02-20 11:13:37 +0200

    object with setters

--- 01_Feb20/lab01.py
+++ 01_Feb20/lab01.py
@@ -1,5 +1,10 @@
 # негарний варіант класа
 class QuadraticEquation:
+    def set_data(self, coef_a, coef_b, coef_c):
+        self.a = coef_a
+        self.b = coef_b
+        self.c = coef_c
+
     def solve_q_equation(self):
         D = self.b ** 2 - 4 * self.a * self.c
         if D < 0:
@@ -10,15 +15,11 @@ class QuadraticEquation:
         print(x1, x2)
 
 eq1 = QuadraticEquation()
-eq1.a = 1.0
-eq1.b = -2.0
-eq1.c = 1.0
+eq1.set_data(1.0, -2.0, 1.0)
 
 second_eq = QuadraticEquation()
-second_eq.a = 1.0
-second_eq.b = -2.0
-second_eq.c = 0.0
+second_eq.set_data(1.0, -2.0, 0.0)
 
 eq1.solve_q_equation()
 
-second_eq.solve_q_equation() # second_eq['solve'](second_eq)
+second_eq.solve_q_equation()
