b48e2dfb48c3662e703eed3e9d75115ffaa7a74c
timn <i-wont-tell@you.com>
2024-02-20 11:09:04 +0200

    semi-objects

--- 01_Feb20/lab01.py
+++ 01_Feb20/lab01.py
@@ -1,22 +1,24 @@
-# class QuadraticEquation:
-#     def __init__(self):
-#
-# прогоарам без класів і об'єктів, яка тим не менше слідує (деяким) принципам ООП
-def solve_q_equation(data):
-    D = data['b'] ** 2 - 4 * data['a'] * data['c']
-    if D < 0:
-        print('No solutions')
-        return
-    x1 = (-data['b'] + D**0.5)/(2 * data['a'])
-    x2 = (-data['b'] - D ** 0.5) / (2 * data['a'])
-    print(x1, x2)
-#
+# негарний варіант класа
+class QuadraticEquation:
+    def solve_q_equation(self):
+        D = self.b ** 2 - 4 * self.a * self.c
+        if D < 0:
+            print('No solutions')
+            return
+        x1 = (-self.b + D ** 0.5) / (2 * self.a)
+        x2 = (-self.b - D ** 0.5) / (2 * self.a)
+        print(x1, x2)
 
-eq1 = {'a': 1.0, 'b': -2.0, 'c': 1.0,
-       'solve': solve_q_equation}
-second_eq = {'a': 1.0, 'b': -2.0, 'c': 0.0,
-             'solve': solve_q_equation}
+eq1 = QuadraticEquation()
+eq1.a = 1.0
+eq1.b = -2.0
+eq1.c = 1.0
 
-eq1['solve'](eq1)
+second_eq = QuadraticEquation()
+second_eq.a = 1.0
+second_eq.b = -2.0
+second_eq.c = 0.0
 
-second_eq['solve'](second_eq)
+eq1.solve_q_equation()
+
+second_eq.solve_q_equation() # second_eq['solve'](second_eq)
