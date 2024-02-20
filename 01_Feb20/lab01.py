9b6347be21b727f9b3989db204ae74ae0073d0e2
timn <i-wont-tell@you.com>
2024-02-20 11:52:35 +0200

    copy constructor added

--- 01_Feb20/lab01.py
+++ 01_Feb20/lab01.py
@@ -1,8 +1,15 @@
 class QuadraticEquation:
-    def __init__(self, coef_a, coef_b, coef_c):
-        self.a = coef_a
-        self.b = coef_b
-        self.c = coef_c
+    def __init__(self, coef_a, coef_b = None, coef_c = None):
+        if isinstance(coef_a, QuadraticEquation):
+            print("We're taking data from existing object")
+            self.a = coef_a.a
+            self.b = coef_a.b
+            self.c = coef_a.c
+        else:
+            print("We're operating on numbers")
+            self.a = coef_a
+            self.b = coef_b
+            self.c = coef_c
 
     def solve_q_equation(self):
         D = self.b ** 2 - 4 * self.a * self.c
@@ -15,7 +22,7 @@ class QuadraticEquation:
 
 eq1 = QuadraticEquation(1.0, -2.0, 1.0)
 
-second_eq = QuadraticEquation(1.0, -2.0, 0.0)
-
+second_eq = QuadraticEquation(eq1)  # конструктро копіювання має взяти всі дані з eq1
+#
 eq1.solve_q_equation()
 second_eq.solve_q_equation()
