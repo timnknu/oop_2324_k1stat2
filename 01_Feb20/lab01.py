90f0b6d3edf9df095cc9b183043274044c91ece4
timn <i-wont-tell@you.com>
2024-02-20 11:35:34 +0200

    constructor added

--- 01_Feb20/lab01.py
+++ 01_Feb20/lab01.py
@@ -1,5 +1,7 @@
-# негарний варіант класа
 class QuadraticEquation:
+    def __init__(self):
+        print('New QuadraticEquation is being created')
+
     def set_data(self, coef_a, coef_b, coef_c):
         self.a = coef_a
         self.b = coef_b
@@ -14,15 +16,10 @@ class QuadraticEquation:
         x2 = (-self.b - D ** 0.5) / (2 * self.a)
         print(x1, x2)
 
-
 eq1 = QuadraticEquation()
-# прямий доступ до даних (потенційно небезпечно)
-eq1.a = 1.0
-eq1.b = -2.0
-eq1.c = 1.0
+eq1.set_data(1.0, -2.0, 1.0)
 
 second_eq = QuadraticEquation()
-# доступ до даних об'єкта під "наглядом" створеного нами "посередника"
 second_eq.set_data(1.0, -2.0, 0.0)
 
 eq1.solve_q_equation()
