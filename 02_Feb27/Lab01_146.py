2927d6a279ef23a904e4ce350c2001c048551e69
timn <i-wont-tell@you.com>
2024-02-27 11:38:49 +0200

    file input + show

--- 02_Feb27/Lab01_146.py
+++ 02_Feb27/Lab01_146.py
@@ -1,21 +1,31 @@
-poly = {}
-with open('input01.txt') as f:
-    for line in f:
-        data = line.strip().split()
-        if len(data):
-            assert len(data)==2
-            try:
-                pwr = int(data[0])
-            except:
-                print('Incorrect power:', line[0])
-                continue
-            #
-            assert pwr >= 0
-            try:
-                coef = float(data[1])
-            except:
-                print('Incorrect coef:', line[1])
-                continue
-            #
-            poly[pwr] = coef
-print(poly)
+class Polynom:
+    def __init__(self):
+        self._data = {}
+    def read_from_file(self, file_name):
+        poly = {}
+        with open(file_name) as f:
+            for line in f:
+                data = line.strip().split()
+                if len(data):
+                    assert len(data)==2
+                    try:
+                        pwr = int(data[0])
+                    except:
+                        print('Incorrect power:', line[0])
+                        continue
+                    #
+                    assert pwr >= 0
+                    try:
+                        coef = float(data[1])
+                    except:
+                        print('Incorrect coef:', line[1])
+                        continue
+                    #
+                    poly[pwr] = coef
+        self._data = poly
+    def show(self):
+        print(self._data)
+
+obj = Polynom()
+obj.read_from_file('input01.txt')
+obj.show()
