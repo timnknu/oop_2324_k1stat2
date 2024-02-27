305901f980bf111601044db7cdfc3f0cd73d8bad
timn <i-wont-tell@you.com>
2024-02-27 11:07:27 +0200

    asserts and try/except added

--- 02_Feb27/Lab01_146.py
+++ 02_Feb27/Lab01_146.py
@@ -3,6 +3,17 @@ with (open('input01.txt') as f):
         data = line.strip().split()
         if len(data):
             assert len(data)==2
-            pwr = int(data[0])
-            coef = float(data[1])
+            try:
+                pwr = int(data[0])
+            except:
+                print('Incorrect power:', line[0])
+                continue
+            #
+            assert pwr >= 0
+            try:
+                coef = float(data[1])
+            except:
+                print('Incorrect coef:', line[1])
+                continue
+            #
             print(pwr, coef)
