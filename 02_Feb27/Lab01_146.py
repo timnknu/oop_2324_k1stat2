f3088cf896024b3e1ed870646a1f80657aeaee91
timn <i-wont-tell@you.com>
2024-02-27 12:10:38 +0200

    semi-getters

--- 02_Feb27/Lab01_146.py
+++ 02_Feb27/Lab01_146.py
@@ -1,6 +1,16 @@
 class Polynom:
     def __init__(self):
         self._coefs_dict = {}
+    #def get_power(self):
+    #    return self._coefs_dict.keys()
+    def get_coef(self, pwr):
+        #return self._coefs_dict.get(pwr, 0)
+        if pwr in self._coefs_dict:
+            return self._coefs_dict[pwr]
+        else:
+            return 0.0
+    #
+
     def _process_line(self, line):
         data = line.split()
         if len(data):
@@ -52,9 +62,17 @@ class Polynom:
     def show(self):
         print(self._coefs_dict)
 
+    @staticmethod
+    def add(poly1, poly2):
+        powers1 = poly1._coefs_dict.keys()
+        powers2 = poly2._coefs_dict.keys()
+        for pwr in set(powers1) | set(powers2):
+            coef = poly1.get_coef(pwr) + poly2.get_coef(pwr)
+
 obj = Polynom()
 obj.read_from_file('input01.txt')
 #obj.read_from_keyboard()
-val = obj.evaluate_at_point(0.5)
-print(val)
-obj.show()
+#val = obj.evaluate_at_point(0.5)
+#print(val)
+#obj.show()
+Polynom.add(obj, obj)
