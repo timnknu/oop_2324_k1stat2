f3c95f407d354923ca5e52a6c2cf5fc572484b19
timn <i-wont-tell@you.com>
2024-03-05 11:55:12 +0200

    all cases in solve, for quadr.eq

--- 03_Mar05/main.py
+++ 03_Mar05/main.py
@@ -23,6 +23,19 @@ class QuadraticEquation(Equation):
     def solve(self):
         if self._a == 0:
             return super().solve()
+        else:
+            D = self._b ** 2 - 4 * self._a * self._c
+            if D < 0:  # розв'язків немає
+                return Equation.NO_SOLS
+            if abs(D) < 1e-10:  # щоб уникнути похибок заокруглення
+                # розв'язок один
+                return (-self._b / (2 * self._a), )
+            else:
+                # розв'язків два
+                x1 = (-self._b + D ** 0.5) / (2 * self._a)
+                x2 = (-self._b - D ** 0.5) / (2 * self._a)
+                return (x1, x2)
+    #
 
 
 if __name__ == "__main__":
