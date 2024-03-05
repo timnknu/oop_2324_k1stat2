1ce42a510e2f5d0575e15df75fb2889708b9af86
timn <i-wont-tell@you.com>
2024-03-05 12:14:55 +0200

    BiQuadraticEquation implemented

--- 03_Mar05/main.py
+++ 03_Mar05/main.py
@@ -1,8 +1,30 @@
 from linear_equation import Equation
 from quadr_equation import QuadraticEquation
 
+class BiQuadraticEquation(QuadraticEquation):
+    def solve(self):
+        ses = super().solve()
+        if ses == Equation.NO_SOLS:
+            return Equation.NO_SOLS
+        #
+        if ses == Equation.INF_SOLS:
+            return Equation.INF_SOLS
+        #
+        result = []
+        for xsq in ses:
+            if xsq >= 0:
+                result.append(xsq**0.5)
+                result.append(- xsq**0.5)
+        #
+        if len(result) == 0:
+            return Equation.NO_SOLS
+        #
+
+        return tuple(result)
 
 if __name__ == "__main__":
-    e = QuadraticEquation(1.0, -2.0, -4)
+    e = BiQuadraticEquation(1.0, -3.0, 2) # (x**2 - 1)*(x**2 - 2)
+    #e = BiQuadraticEquation(1.0, -2.0, -1)  # (x**2 + 1)*(x**2 - 2)
+    #e = BiQuadraticEquation(1.0, 3.0, 2)  # (x**2 + 1)*(x**2 + 2)
     r = e.solve()
     print(r)
