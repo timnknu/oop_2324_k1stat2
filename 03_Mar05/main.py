dea2cbb740765dd09bc815518b83d76c318b361f
timn <i-wont-tell@you.com>
2024-03-05 11:35:34 +0200

    class Equation+tests

--- 03_Mar05/main.py
+++ 03_Mar05/main.py
@@ -1,14 +1,29 @@
 class Equation:
-    INF_SOLS = -1
+    INF_SOLS = (None, None, None, None, None)
+    NO_SOLS = tuple()
+
     def __init__(self, b, c):
         self._b = b
         self._c = c
     def solve(self):
         # bx + c =  0 => x = -c/b
-        #(None, None, None, None, None)
-        pass
+        if self._b==0:
+            if self._c==0:
+                return Equation.INF_SOLS
+            else:
+                return Equation.NO_SOLS
+        else:
+            return (- self._c / self._b, ) # кортеж з одного елемента
 
-print(Equation.INF_SOLS) # доступ до статичного поля (через ім'я класу)
+if __name__ == "__main__":
+    e = Equation(1.0, 2.0)
+    r = e.solve()
+    assert r==(-2.0/1.0, )
 
-e = Equation(1.0, 2.0)
-print(e.INF_SOLS) # доступ до статичного поля (через об'єкт) -- краще не робити так
+    e = Equation(0.0, 2.0)
+    r = e.solve()
+    assert r==Equation.NO_SOLS
+
+    e = Equation(0.0, 0.0)
+    r = e.solve()
+    assert r==Equation.INF_SOLS
