75a777c7083b1da048a4ed40572c9af92717d44e
Your Name <you@example.com>
2024-03-15 18:08:26 +0200

    turtle-ex7

--- 04_Mar12/turtle_intro.py
+++ 04_Mar12/turtle_intro.py
@@ -5,14 +5,23 @@ turtle.speed("slowest")
 turtle.pendown()
 
 a = 0
-n = 3
+n = 10
+
 for i in range(n):
     turtle.setheading(a)
     turtle.forward(100)
     a += 360/n
+
+turtle.color(turtle.bgcolor())
+
+for i in range(n):
+    turtle.setheading(a)
+    turtle.forward(100)
+    a += 360/n
+
 turtle.penup()
 
-turtle.color('red')
+turtle.color('#CD853F')
 turtle.goto(-100, -100)
 turtle.pendown()
 turtle.goto(-100, -50)
