f38fecb3d7fefeb3b1bd385e2c98ba94d8a71596
Your Name <you@example.com>
2024-03-15 17:50:52 +0200

    turtle-ex4

--- 04_Mar12/turtle_intro.py
+++ 04_Mar12/turtle_intro.py
@@ -1,13 +1,15 @@
 import turtle
 
 turtle.pendown()
-turtle.forward(150)
-turtle.setheading(45)
+
+turtle.setheading(0)
 turtle.forward(150)
 
-turtle.setheading(90)
+turtle.setheading(120)
 turtle.forward(150)
 
+turtle.setheading(240)
+turtle.forward(150)
 
 #turtle.mainloop()
 turtle.exitonclick()
