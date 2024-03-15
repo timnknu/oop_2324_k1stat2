1356e7969dd034c470b3c7e970ca670569504a17
Your Name <you@example.com>
2024-03-15 18:04:32 +0200

    turtle-ex6

--- 04_Mar12/turtle_intro.py
+++ 04_Mar12/turtle_intro.py
@@ -1,13 +1,22 @@
 import turtle
 
+turtle.speed("slowest")
+
 turtle.pendown()
 
 a = 0
-n = 10
+n = 3
 for i in range(n):
     turtle.setheading(a)
     turtle.forward(100)
     a += 360/n
+turtle.penup()
+
+turtle.color('red')
+turtle.goto(-100, -100)
+turtle.pendown()
+turtle.goto(-100, -50)
+turtle.goto(-50, -50)
 
 
 #turtle.mainloop()
