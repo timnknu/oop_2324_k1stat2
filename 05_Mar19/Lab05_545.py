cdd898b771ab9c815aa22f47d3c5a7075a9894ae
timn <i-wont-tell@you.com>
2024-03-19 11:47:15 +0200

    __add__ demo

--- 05_Mar19/Lab05_545.py
+++ 05_Mar19/Lab05_545.py
@@ -12,15 +12,20 @@ class Vector:
     def __getitem__(self, j):
         return self._data[j]
     def __setitem__(self, j, val):
+        # тут можуть бути додаткові перевірки
         self._data[j] = val
     def __str__(self):
         s = 'Vec: '
         s += ', '.join(map(str, self._data))
         return s
+    def __add__(self, other):
+        print('other', other)
+        return 'HH'
 
 
 a = Vector(3)
 print(a[0])
 a[0] = 1.2
+a = a + 1
 print(a)
 
