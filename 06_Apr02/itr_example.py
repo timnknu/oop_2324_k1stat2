f96e86f227fd6669d8e8216eed28a3f91aa7ba11
timn <i-wont-tell@you.com>
2024-04-02 11:46:15 +0300

    StopIteration added

--- 06_Apr02/itr_example.py
+++ 06_Apr02/itr_example.py
@@ -1,18 +1,24 @@
-class SqNumCreator:
-    def __init__(self):
-        self._storage = ['a', 'y', 'b', -10]
+class SqNumCreator: # iterator
+    def __init__(self, nfinal):
         self._k = 0
+        self._max_k = nfinal
     def __next__(self):
-        elem = self._storage[self._k]
+        #print('__next__ is called', self)
         self._k += 1
-        return elem
+        if self._k < self._max_k:
+            return self._k**2
+        else:
+            raise StopIteration
 
-class MyClass:
+class MyClass: # iteratable
+    def __init__(self, n):
+        self._max_num = n
     def __iter__(self):
-        return SqNumCreator()
+        #print('>> __iter__ called from', self)
+        return SqNumCreator(self._max_num)
 
 if __name__ == "__main__":
-    sq = MyClass()
+    sq = MyClass(3)
 
     for num in sq: # b1 = iter(sq)
         # num = next(b1)
