be20436cd8529a29b47a2f71c85ebc0b1a0fd0ca
timn <i-wont-tell@you.com>
2024-04-02 11:24:49 +0300

    problems of __iter__ and __next__ in the same class

--- 06_Apr02/itr_example.py
+++ 06_Apr02/itr_example.py
@@ -1,15 +1,26 @@
-
 class MyClass:
+    def __init__(self):
+        self._storage = ['a', 'y', 'b', -10]
+        self._k = 0
     def __iter__(self):
-        #return 'XYZ'
-        #pass
-        return iter(['a', 'y', 'b', -10])
-
+        return self
+    def __next__(self):
+        elem = self._storage[self._k]
+        self._k += 1
+        return elem
 
 if __name__ == "__main__":
-
     sq = MyClass()
 
-    for num in sq: # iter(sq) -> b
-        # next(b) -> num
-        print(num)
+    for num in sq: # b1 = iter(sq)
+        # num = next(b1)
+        for el in sq: # b2 = iter(sq)
+            # el = next(b2)
+            print(num, el)
+
+
+
+
+# class SqNumCreator:
+#     def __next__(self):
+#         return -10
