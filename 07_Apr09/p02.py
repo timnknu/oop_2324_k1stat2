6c02570c80f9e93234fa641029b09a60658b420f
timn <i-wont-tell@you.com>
2024-04-09 11:54:15 +0300

    8.2.1(e), with sum

--- 07_Apr09/p02.py
+++ 07_Apr09/p02.py
@@ -1,16 +1,21 @@
-def seqgen(x):
+import math
+
+def seqgen(x, eps):
     a = 1.
     n = 1
     yield a
-    while True:
+    while a > eps:
         a = a * x / n
         n += 1
         yield a
 #
 
-N = 11
-v = seqgen(0.12)
-b = iter(v)
-for k in range(N):
-    el = next(b)
-    print(el)
+x = 1.2
+
+v = seqgen(x, 1e-3)
+y = 0.0
+for el in v:
+    y += el
+    print(el, 'y =',y)
+
+print('math:', math.exp(x))
