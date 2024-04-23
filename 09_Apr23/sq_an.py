42279d3263d69eda110d66476604b74913945235
timn <i-wont-tell@you.com>
2024-04-23 11:52:50 +0300

    x-grid in print_table

--- 09_Apr23/sq_an.py
+++ 09_Apr23/sq_an.py
@@ -1,4 +1,5 @@
 import abc
+import math
 
 class SequenceAnalyzer(metaclass=abc.ABCMeta):
     @abc.abstractmethod
@@ -13,6 +14,17 @@ class SequenceAnalyzer(metaclass=abc.ABCMeta):
             if k > N:
                 break
         return s
+
+    def eval(self, x):
+        eps = 1e-5 # те саме, що і 10**(-5)
+        sm = 0
+        for k, ak in self.terms_gen():
+            term = ak * x**k
+            if abs(term) < eps:
+                break
+            sm += term
+        return sm
+
 
 class SinSeq(SequenceAnalyzer):
     def terms_gen(self):
@@ -35,8 +47,27 @@ class CosSeq(SequenceAnalyzer):
             n += 2
             yield (n, a)
 
+
+def print_table(objs, a=0, b=1, npoints=10):
+    # a    == x[1]
+    # a+s  == x[2]
+    # a+2s == x[3]
+    # ...
+    # b    == x[npoints] = a + s*(npoints-1)
+    s = (b-a)/(npoints-1)
+    # x[i] == a + s*(i-1)
+    for i in range(1, npoints+1):
+        x = a + s*(i-1)
+        print(f"x_{i} = {x}: (f1, f2, ...)")
+#
+
+
+print_table([], a=-1, b=1)
+print('----')
+
 if __name__ == "__main__":
     #s = SinSeq()
     s = CosSeq()
-    print(s)
+    print(s.eval(2.0))
+    print(math.cos(2.0))
 
